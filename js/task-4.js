cab17df239c991ea65300691ed4b69fd3bd0f750
Plane-Jumper5 <romariobelyaev@gmail.com>
2024-04-25 12:52:04 +0300

    fix

--- js/task-4.js
+++ js/task-4.js
@@ -1,9 +1,51 @@
 const getTotalBalanceByGender = (users, gender) => {
   return users
-    .filter(user => user.gender === gender) // Фільтруємо користувачів за статтю
-    .reduce((totalBalance, user) => totalBalance + user.balance, 0); // Знаходимо загальний баланс за допомогою reduce()
+    .filter(user => user.gender === gender)
+    .reduce((totalBalance, user) => totalBalance + user.balance, 0);
 };
 
-console.log(getTotalBalanceByGender(users, 'male')); // Поверне загальний баланс чоловіків (100 + 300 = 400)
+const clients = [
+  {
+    name: "Moore Hensley",
+    gender: "male",
+    balance: 2811
+  },
+  {
+    name: "Sharlene Bush",
+    gender: "female",
+    balance: 3821
+  },
+  {
+    name: "Ross Vazquez",
+    gender: "male",
+    balance: 3793
+  },
+  {
+    name: "Elma Head",
+    gender: "female",
+    balance: 2278
+  },
+  {
+    name: "Carey Barr",
+    gender: "male",
+    balance: 3951
+  },
+  {
+    name: "Blackburn Dotson",
+    gender: "male",
+    balance: 1498
+  },
+  {
+    name: "Sheree Anthony",
+    gender: "female",
+    balance: 2764
+  }
+];
+
+console.log(getTotalBalanceByGender(clients, "male")); // Поверне загальний баланс користувачів чоловічої статі
+console.log(getTotalBalanceByGender(clients, "female")); // Поверне загальний баланс користувачів жіночої статі
+
+
+
 
 
